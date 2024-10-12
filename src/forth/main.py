49282d98c33c854b551f8bd6f528330295275dd2
Markus Holmström (MawKKe) <markus@mawkke.fi>
2024-10-12 20:44:36 +0300

    implement support for inline source (-c)

--- src/forth/main.py
+++ src/forth/main.py
@@ -6,23 +6,37 @@ import typing as t
 import forth
 
 
-def chain_files(files: list) -> t.Iterator[str]:
-    for file in files:
+def chain_files(open_files: list) -> t.Iterator[str]:
+    for file in open_files:
         yield from forth.gen_tokens_from_line_iterable(file)
 
 
 def main(argv: list[str]) -> int:
     p = argparse.ArgumentParser(prog=Path(argv[0]).name)
-    p.add_argument('sources', nargs='+', metavar='src', type=argparse.FileType('r'))
+    p.add_argument('-c', type=str, dest='inline_src', metavar='inline-src')
+    p.add_argument('file_src', metavar='file-src', nargs='*', type=argparse.FileType('r'))
     p.add_argument('--show-stats', action='store_true')
     args = p.parse_args(argv[1:])
 
     vm = forth.VM()
     vm = forth.ops.register_default_ops(vm)
 
-    token_stream = chain_files(args.sources)
+    if args.inline_src and args.file_src:
+        print(
+            'error: inline source (-c) and file source(s) specified at the same time',
+            file=sys.stderr,
+        )
+        return -2
 
-    vm.eval_token_stream(token_stream)
+    if args.inline_src:
+        vm.eval_string(args.inline_src)
+    elif args.file_src:
+        token_stream = chain_files(args.file_src)
+
+        vm.eval_token_stream(token_stream)
+    else:
+        print('error: no sources provided', file=sys.stderr)
+        return -1
 
     if args.show_stats:
         print('# counters:', vm.get_counters())
--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -9,6 +9,14 @@ def test_main_help() -> None:
     assert exc_info.value.code == 0
 
 
+def test_main_no_args(capfdbinary, tmp_path) -> None:  # type: ignore
+    rc = forth.main.main(['forth'])
+    assert rc == -1
+    out, err = capfdbinary.readouterr()
+    assert out == b''
+    assert err.startswith(b'error: no sources')
+
+
 def test_main_1file(capfdbinary, tmp_path) -> None:  # type: ignore
     example = tmp_path / 'example.forth'
     assert not example.exists()
@@ -30,6 +38,30 @@ def test_main_stdin_as_input_file(monkeypatch, capfdbinary):  # type: ignore
     assert out == b'42\n'
 
 
+def test_main_inline_src(capfdbinary):  # type: ignore
+    src = '21 2 * . CR'
+
+    rc = forth.main.main(['forth', '-c', src])
+
+    assert rc == 0
+
+    out, err = capfdbinary.readouterr()
+    assert out == b'42\n'
+    assert err == b''
+
+
+def test_main_inline_src_and_file_src_should_error(tmp_path, capfdbinary):  # type: ignore
+    src = tmp_path / 'dummy'
+    src.touch()
+    rc = forth.main.main(['forth', '-c', 'inline-src', src.as_posix()])
+
+    assert rc == -2
+
+    out, err = capfdbinary.readouterr()
+    assert out == b''
+    assert err.startswith(b'error:')
+
+
 def test_main_halt(tmp_path, capfdbinary):  # type: ignore
     src1 = tmp_path / 'src1.forth'
     src1.write_bytes(b'1 1 + . CR HALT')
@@ -43,14 +75,14 @@ def test_main_halt(tmp_path, capfdbinary):  # type: ignore
 
 
 def test_show_stats(monkeypatch, capfdbinary):  # type: ignore
-    monkeypatch.setattr('sys.stdin', io.StringIO('21 2 * . CR'))
+    src = '21 2 * . CR'
 
-    forth.main.main(['forth', '-'])
+    forth.main.main(['forth', '-c', src])
 
     out, _ = capfdbinary.readouterr()
     assert b"# counters: {'tokens_processed':" not in out
 
-    forth.main.main(['forth', '-', '--show-stats'])
+    forth.main.main(['forth', '-c', src, '--show-stats'])
 
     out, _ = capfdbinary.readouterr()
     assert b"# counters: {'tokens_processed':" in out
